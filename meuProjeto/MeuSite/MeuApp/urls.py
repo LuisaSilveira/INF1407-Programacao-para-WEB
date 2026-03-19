b227143dabecc37e9ef07bcd95c0eb8c7c7c661d
Luisa Ferreira da Silveira <112108551+LuisaSilveira@users.noreply.github.com>
2026-03-19 11:50:15 +0000

    site funcionando com django

--- meuProjeto/MeuSite/MeuApp/urls.py
+++ meuProjeto/MeuSite/MeuApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from MeuApp import views
 
+app_name = "MeuApp"
+
 urlpatterns = [
     path("", views.home, name="home"),
+    path("segundaPagina/", views.segundaPagina, name="segundaPagina"),
 ]
--- meuProjeto/MeuSite/MeuApp/views.py
+++ meuProjeto/MeuSite/MeuApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return HttpResponse("Olá, mundo! Esta é a página inicial do MeuApp.", content_type="text/plain")
+    return render(request, "MeuApp/home.html")
+
+def segundaPagina(request):
+    return render(request, "MeuApp/segundaPagina.html")
